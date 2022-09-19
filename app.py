f040fa69b6626ff31f64137d73c85441a6a49a61
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-09-19 23:20:20 +0530

    vars changed

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 if __name__ == '__main__':
     try:
-        s=Singleton()
-        print(s.get_client())
+        dbconnection=Singleton()
+        print(f"Connection successful:{dbconnection.get_client()}")
     except Exception as e:
         print(e)
 
