2c6d55751bda5978a48ac9a84d0d74437f20c186
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2022-09-23 19:32:46 +0530

    Add Full data reception script

--- API/utils/DBQueries.py
+++ API/utils/DBQueries.py
@@ -8,11 +8,10 @@ class DBQueries():
     def insert_to_database(cls, db_name, coll_name, data):
         """insert records"""
         con = DBConnection.get_client()
-        print(con)
         mydb = con[db_name]
         mycol = mydb[coll_name]
 
-        if type(data) is list:
+        if isinstance(data, list):
             return mycol.insert_many(data)
         else:
             return mycol.insert_one(data)
@@ -21,6 +20,17 @@ class DBQueries():
     @classmethod
     def count_all_documents(cls, db_name, coll_name):
         """count dcouments in collection"""
+        con = DBConnection.get_client()
+
         mydb = DBQueries.con.client[db_name]
         mycol = mydb[coll_name]
         return mycol.count_documents({})
+
+    @classmethod
+    def fetch_last(cls, db_name, coll_name):
+        con = DBConnection.get_client()
+
+        mydb = con[db_name]
+        mycol = mydb[coll_name]
+
+        return mycol.find().sort('_id', -1)[0]
