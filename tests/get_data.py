87cc281f67e3e297b752fbcc3fe51626c932daaa
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-09-26 23:07:13 +0530

    test getfromdb

--- tests/get_data.py
+++ tests/get_data.py
@@ -1,16 +1,16 @@
 import unittest
-from API.services.DBManipulation import fetch_from_db
+# from API.services.DBManipulation import fetch_from_db
 
 
 
 class MyGetTestCase(unittest.TestCase):
-    def get_something(self):
+    def test_get_fromdb(self):
         import requests
 
         url = "http://localhost:8000/api/get_data"
-        retreive=requests.get(url)
-        print(retreive.text)
-        self.assertEqual(retreive.text,"200")
+        retreive=requests.get(url,'string')
+        # print(retreive.json()["detail"])
+        self.assertEqual(isinstance(retreive.json(),dict),True)
 
 if __name__=="__main__":
     unittest.main()        
--- tests/post_data.py
+++ tests/post_data.py
@@ -2,7 +2,7 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_post2db(self):
         import requests
 
         data = {
