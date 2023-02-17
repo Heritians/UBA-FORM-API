16ddb174da8b7593fd3df8a240ab5432853c44cf
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2023-02-17 14:52:26 +0530

    Update test cases to check intended responses

--- tests/test_fetch_indi_data.py
+++ tests/test_fetch_indi_data.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import requests
+import json
 from login_utils import get_access_token, BASE_URL
 
 
@@ -14,7 +15,7 @@ class TestFetchIndividualData(unittest.TestCase):
         "role": f"{os.environ['OWNER_ROLE']}"
         }
 
-    params = {"respondents_id": "305040848937547"}
+    params = {"respondents_id": "test_sub"}
 
     def test_owner(self):
         
@@ -29,14 +30,15 @@ class TestFetchIndividualData(unittest.TestCase):
 
     def test_admin(self):
         TestFetchIndividualData.signincred['role'] = os.environ['ADMIN_ROLE']
-        params = {"respondents_id": "2345"}
         headers = {
             "accept": "application/json",
             "Authorization": f"Bearer {get_access_token(TestFetchIndividualData.signincred)}",
             "Content-Type": "application/json"
         }
-        response = requests.get(url=TestFetchIndividualData.url, params=params, headers=headers)
-        self.assertEqual(response.json()['status'], "success")
+        response = requests.get(url=TestFetchIndividualData.url, params=TestFetchIndividualData.params, headers=headers)
+        with open("intended responses/fetch_indiv_data.json", 'r') as f:
+            data = json.load(f)
+            self.assertEqual(response.json(), data)
 
     def test_user(self):
         TestFetchIndividualData.signincred['role'] = os.environ['USER_ROLE']
