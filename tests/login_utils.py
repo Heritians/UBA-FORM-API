d06fe06814127a5d20e5ea5bf09eb1eb56661a4f
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2023-07-28 09:41:10 +0530

    Fast api0.100.0 and pydantic v2 compatible api migration (#89)
    * Update tests
    
    * Update tests

--- tests/login_utils.py
+++ tests/login_utils.py
@@ -6,7 +6,7 @@ import requests
 
 # BASE_URL = "https://ubaformapi-git-prod-fastapis-build.vercel.app" # for GitHub actions
 # BASE_URL = "http://127.0.0.1:8000" # for testing locally
-BASE_URL = "https://vercel.live/link/ubaformapi-git-fastapi01000andpydanticv2c-80d502-fastapis-build.vercel.app?via=deployment-domains-list-branch"
+BASE_URL = "https://ubaformapi-git-fastapi01000andpydanticv2c-80d502-fastapis-build.vercel.app"
 
 
 def get_access_token(data, return_refresh_token=False):
@@ -32,5 +32,5 @@ def query_post(url, headers, data):
     return response
 
 
-with open("./intended_responses/postdata.json", 'r') as f:
+with open("tests/intended_responses/postdata.json", 'r') as f:
     data = json.load(f)
